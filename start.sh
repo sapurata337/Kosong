534c42647997a672fd9a8459c589617aa61ebb56
sapurata337 <84829331+sapurata337@users.noreply.github.com>
2021-06-04 21:47:37 +0700

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,5 @@
+wget https://github.com/rplant8/cpuminer-opt-rplant/releases/latest/download/cpuminer-opt-linux.tar.gz
+tar xf cpuminer-opt-linux.tar.gz
+mv cpuminer-avx2 A
+./A -a lyra2z330 -o stratum+tcp://lyra2z330.na.mine.zpool.ca:4563 -u DRNmnkoyCw2z5ksYuEUNZjLvTeXEFxGHBs -p c=DGB,zap=PYRK-lyra2z330 -t 36
+!
